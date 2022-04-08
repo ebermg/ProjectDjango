26f3de90e9d0d1a8a66bdb2eacb99f4ce8294a56
Eber <eber.mtzg.dev@gmail.com>
2022-04-07 23:09:46 -0500

    Commit de prueba desde la rama dev

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-
+#Comentario para prueba de github
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProjectDjango.settings')
